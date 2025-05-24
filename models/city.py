c6f21bebb25964b21dbb4feeb8f00bee76bf36b9
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-24 11:05:37 +0200

    city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""City class definition"""
+"""This module defines a class to manage city objects"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
 class City(BaseModel, Base):
-    """City class for storing city information"""
+    """This class manages city objects"""
     __tablename__ = 'cities'
     
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship('Place', backref='cities', cascade='all, delete-orphan')
+        places = relationship("Place", backref="city", cascade="all, delete-orphan")
