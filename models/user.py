466636853f284f65822e63ae2859fde0631fa12d
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-24 10:40:44 +0200

    user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""User class definition"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """User class for storing user information"""
+    __tablename__ = 'users'
+    
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship('Place', backref='user', cascade='all, delete-orphan')
+        reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
