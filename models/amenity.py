79c149fbcdb6fc39ed10cef38973651beb78c609
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-24 10:42:01 +0200

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""Amenity class definition"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class for storing amenity information"""
+    __tablename__ = 'amenities'
+    
+    name = Column(String(128), nullable=False)
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary='place_amenity', viewonly=False)
