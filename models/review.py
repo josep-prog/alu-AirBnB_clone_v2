34fcf939449ddc3ff2a4ea6a3f73074f9ae81808
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-25 22:51:30 +0200

    review.py

--- models/review.py
+++ models/review.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Review class definition"""
+"""This module defines a class to manage review objects"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 class Review(BaseModel, Base):
-    """Review class for storing review information"""
+    """This class manages review objects"""
     __tablename__ = 'reviews'
     
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place = relationship('Place', backref='reviews', cascade='all, delete-orphan')
+        user = relationship('User', backref='reviews', cascade='all, delete-orphan')
