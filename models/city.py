ea27b9980b6652ecdba4ad5c8b83d1b1190ecf2b
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-25 22:50:16 +0200

    city.py

--- models/city.py
+++ models/city.py
@@ -11,4 +11,17 @@ class City(BaseModel, Base):
     
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="city", cascade="all, delete-orphan")
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship('Place', backref='city', cascade='all, delete-orphan')
+    else:
+        @property
+        def places(self):
+            """Getter for places"""
+            from models import storage
+            from models.place import Place
+            place_list = []
+            for place in storage.all(Place).values():
+                if place.city_id == self.id:
+                    place_list.append(place)
+            return place_list
