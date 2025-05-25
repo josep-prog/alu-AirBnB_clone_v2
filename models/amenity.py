dfadda144103ea2c6aa482fcc13d99b1be3646f8
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-25 23:11:22 +0200

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""Amenity class definition"""
+"""This module defines a class to manage amenity objects"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
 class Amenity(BaseModel, Base):
-    """Amenity class for storing amenity information"""
+    """This class manages amenity objects"""
     __tablename__ = 'amenities'
     
     name = Column(String(128), nullable=False)
     
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship('Place', secondary='place_amenity', viewonly=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                     back_populates='amenities')
+    else:
+        @property
+        def place_amenities(self):
+            """Getter for place_amenities"""
+            from models import storage
+            from models.place import Place
+            place_list = []
+            for place in storage.all(Place).values():
+                if self.id in place.amenity_ids:
+                    place_list.append(place)
+            return place_list
