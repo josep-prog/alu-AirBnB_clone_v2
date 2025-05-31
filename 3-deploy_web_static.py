3dc7fcf7741b172f05a2f4192c6c4e5941671cba
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-31 16:17:48 +0200

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,54 +1,90 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
-
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+Fabric script to create and distribute an archive to web servers
 """
 
-from fabric.api import env, local, put, run
+from fabric.api import env, local, put, run, sudo
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['52.1.22.58', '54.235.17.2']
 
+# Server configurations
+env.hosts = ['52.71.25.46', '54.86.41.67']  # web-01 and web-02
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_pack():
-    """generates a tgz archive"""
+    """Create a tar gzipped archive of the web_static directory"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        if not isdir("versions"):
+            local("mkdir -p versions")
+        file_name = f"versions/web_static_{date}.tgz"
+        local(f"tar -cvzf {file_name} web_static")
         return file_name
-    except:
+    except Exception:
         return None
 
-
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Deploy web_static to web servers
+    
+    Args:
+        archive_path: Path to the archive file
+        
+    Returns:
+        True if deployment is successful, False otherwise
+    """
+    if not exists(archive_path):
         return False
+        
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        # Get archive filename without extension
+        archive_name = archive_path.split('/')[-1]
+        archive_name_no_ext = archive_name.split('.')[0]
+        
+        # Upload archive to /tmp/
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        
+        # Create release directory
+        run(f'sudo mkdir -p /data/web_static/releases/{archive_name_no_ext}/')
+        
+        # Uncompress archive
+        run(f'sudo tar -xzf /tmp/{archive_name} -C /data/web_static/releases/{archive_name_no_ext}/')
+        
+        # Remove archive from /tmp/
+        run(f'sudo rm /tmp/{archive_name}')
+        
+        # Move contents to release directory
+        run(f'sudo mv /data/web_static/releases/{archive_name_no_ext}/web_static/* '
+            f'/data/web_static/releases/{archive_name_no_ext}/')
+        
+        # Remove empty web_static directory
+        run(f'sudo rm -rf /data/web_static/releases/{archive_name_no_ext}/web_static')
+        
+        # Remove current symlink
+        run('sudo rm -rf /data/web_static/current')
+        
+        # Create new symlink
+        run(f'sudo ln -s /data/web_static/releases/{archive_name_no_ext}/ '
+            '/data/web_static/current')
+        
+        # Set proper permissions
+        run('sudo chown -R ubuntu:ubuntu /data/web_static/')
+        
+        # Restart Nginx to ensure changes take effect
+        run('sudo service nginx restart')
+        
+        print("New version deployed!")
         return True
-    except:
+        
+    except Exception as e:
+        print(f"Deployment failed: {str(e)}")
         return False
-
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """Create and distribute an archive to web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
-    
+    return do_deploy(archive_path)
+
